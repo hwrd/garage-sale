330efc4ef986645bc7873b682f1d063136f9a9aa
Brian Howenstein <brian@hwrd.com>
2016-04-18 13:13:56 -0400

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-AdminUser.create!(email: 'brian@circlecitysc.com', password: 'buckfurpees', password_confirmation: 'buckfurpees')
+AdminUser.create!(email: 'brian@circlecitysc.com', password: 'changeme', password_confirmation: 'changeme')
